0bf70dc7eef1619e8224b107a6d3d3d83a1e0f1c
educeciliato <148019576+educeciliato@users.noreply.github.com>
2025-05-08 20:37:23 -0300

    Update listar_medicamentos.dart

--- app_farmacia/app/listar_medicamentos.dart
+++ app_farmacia/app/listar_medicamentos.dart
@@ -1,21 +1,32 @@
 import 'package:flutter/material.dart';
 
 class ListarMedicamentos extends StatelessWidget {
+  const ListarMedicamentos({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Lista de Medicamentos'),
-      ),
+      appBar: AppBar(title: const Text('Lista de Medicamentos')),
       body: ListView.builder(
-        itemCount: 5,
+        itemCount: 5, 
         itemBuilder: (context, indice) {
-          return ListTile(
-            title: Text('Medicamento $indice'),
-            subtitle: Text('Detalhes do Medicamento $indice'),
-          );
+          return MedicamentoTile(indice: indice);
         },
       ),
+    );
+  }
+}
+
+class MedicamentoTile extends StatelessWidget {
+  final int indice;
+
+  const MedicamentoTile({super.key, required this.indice});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text('Medicamento $indice'),
+      subtitle: Text('Detalhes do Medicamento $indice'),
     );
   }
 }
