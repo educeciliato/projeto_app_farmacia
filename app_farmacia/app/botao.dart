57d6a185611af6a42fe898ffee0ca1cef3a5604b
educeciliato <148019576+educeciliato@users.noreply.github.com>
2025-05-08 20:36:13 -0300

    Update botao.dart

--- app_farmacia/app/botao.dart
+++ app_farmacia/app/botao.dart
@@ -1,51 +1,27 @@
 import 'package:flutter/material.dart';
 
-class Botao extends StatefulWidget {
+class Botao extends StatelessWidget {
   final String rotulo;
   final Color cor;
   final VoidCallback aoPressionar;
 
-  Botao({
+  const Botao({
+    super.key,
     required this.rotulo,
     required this.cor,
     required this.aoPressionar,
   });
 
   @override
-  _EstadoBotao createState() => _EstadoBotao();
-}
-
-class _EstadoBotao extends State<Botao> {
-  late Color _corAtual;
-
-  @override
-  void initState() {
-    super.initState();
-    _corAtual = widget.cor;
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-      onEnter: (_) {
-        setState(() {
-          _corAtual = widget.cor.withOpacity(0.8);
-        });
-      },
-      onExit: (_) {
-        setState(() {
-          _corAtual = widget.cor;
-        });
-      },
-      child: ElevatedButton(
-        style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all(_corAtual),
-          padding: MaterialStateProperty.all(EdgeInsets.symmetric(horizontal: 24, vertical: 12)),
-          textStyle: MaterialStateProperty.all(TextStyle(fontSize: 18)),
-        ),
-        onPressed: widget.aoPressionar,
-        child: Text(widget.rotulo),
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: cor,
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+        textStyle: const TextStyle(fontSize: 18),
       ),
+      onPressed: aoPressionar,
+      child: Text(rotulo),
     );
   }
 }
