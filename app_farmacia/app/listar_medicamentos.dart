1b35c9a84c49b83e69bbc1a0cce7d06a349ab2d7
educeciliato <148019576+educeciliato@users.noreply.github.com>
2025-05-29 20:34:23 -0300

    Update listar_medicamentos.dart

--- app_farmacia/app/listar_medicamentos.dart
+++ app_farmacia/app/listar_medicamentos.dart
@@ -1,3 +1,5 @@
+import 'package:aula2905/botao.dart';
+import 'package:aula2905/controle_estoque.dart';
 import 'package:flutter/material.dart';
 
 class ListarMedicamentos extends StatelessWidget {
@@ -7,26 +9,44 @@ class ListarMedicamentos extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Lista de Medicamentos')),
-      body: ListView.builder(
-        itemCount: 5, 
-        itemBuilder: (context, indice) {
-          return MedicamentoTile(indice: indice);
-        },
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Botao(
+              rotulo: 'Remover do Estoque',
+              cor: Colors.red,
+              aoPressionar: () => Navigator.pushNamed(context, '/remover'),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: medicamentos.length,
+              itemBuilder: (context, indice) {
+                final medicamento = medicamentos[indice];
+                return ListTile(
+                  title: Text('${medicamento['nome']} ${medicamento['dose']}'),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                          '${medicamento['tipo']} - ${medicamento['descricao']}'),
+                      Text('Laboratório: ${medicamento['laboratorio']}'),
+                      Text(
+                          'Data de Fabricação: ${medicamento['Data de Fabricacao']}'),
+                      Text(
+                          'Data de Validade: ${medicamento['Data de Validade']}'),
+                      Text(
+                          'Quantidade: ${medicamento['quantidade']} - Lote: ${medicamento['lote']}'),
+                    ],
+                  ),
+                  isThreeLine: true,
+                );
+              },
+            ),
+          ),
+        ],
       ),
-    );
-  }
-}
-
-class MedicamentoTile extends StatelessWidget {
-  final int indice;
-
-  const MedicamentoTile({super.key, required this.indice});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text('Medicamento $indice'),
-      subtitle: Text('Detalhes do Medicamento $indice'),
     );
   }
 }
